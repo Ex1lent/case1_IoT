d518622165e6c0d39cc91c1b133da329bc5f7697
Ex1lent <Roman200-sh@yandex.ru>
2025-10-23 15:03:38 +0300

    test1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "Arduino.h"
+#define BTN1 4
+#define BTN2 5
+#define BTN3 18
+#define BTN4 19
+#define RELE 21
+#define LDR 12
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(BTN1, INPUT_PULLDOWN);
+  pinMode(BTN2, INPUT_PULLDOWN);
+  pinMode(BTN3, INPUT_PULLDOWN);
+  pinMode(BTN4, INPUT_PULLDOWN);
+  pinMode(RELE, OUTPUT);
+  pinMode(LDR, INPUT);
+}
+
+void doorOpen(){
+  digitalWrite(RELE, 1);
+}
+
+void doorClose(){
+  digitalWrite(RELE, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int ldrValue = analogRead(LDR);
+  if (ldrValue > 1500){
+    doorClose();
+  } else {
+    doorOpen();
+  }
+  delay(100);
 }
